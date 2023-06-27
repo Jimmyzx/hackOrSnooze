cf3f6d2317cb1f261b17e9bdd9bf0ccd2b78824a
Jimmyzx <125935094+Jimmyzx@users.noreply.github.com>
2023-06-26 20:30:41 -0700

    init commit

---
+++ js/user.js
@@ -0,0 +1,181 @@
+"use strict";
+
+// global to hold the User instance of the currently-logged-in user
+let currentUser;
+
+/******************************************************************************
+ * User login/signup/login
+ */
+
+/** Handle login form submission. If login ok, sets up the user instance */
+
+
+async function login(evt) {
+  console.debug("login", evt);
+  evt.preventDefault();
+
+  // grab the username and password
+  const username = $("#login-username").val();
+  const password = $("#login-password").val();
+
+  try {
+    // User.login retrieves user info from API and returns User instance
+    // which we'll make the globally-available, logged-in user.
+    currentUser = await User.login(username, password);
+
+    $loginForm.trigger("reset");
+
+    saveUserCredentialsInLocalStorage();
+    updateUIOnUserLogin();
+  } catch (error) {
+    // Handle login error
+    if (error.response.status === 401) {
+      // Incorrect credentials
+      displayErrorMessage("Incorrect username and/or password");
+    } else {
+      // Other error
+      displayErrorMessage("An error has occurred, please try again later.");
+    }
+  }
+}
+
+async function signup(evt) {
+  console.debug("signup", evt);
+  evt.preventDefault();
+
+  const name = $("#signup-name").val();
+  const username = $("#signup-username").val();
+  const password = $("#signup-password").val();
+
+  try {
+    // User.signup retrieves user info from API and returns User instance
+    // which we'll make the globally-available, logged-in user.
+    currentUser = await User.signup(username, password, name);
+
+    saveUserCredentialsInLocalStorage();
+    updateUIOnUserLogin();
+
+    $signupForm.trigger("reset");
+  } catch (error) {
+    // Handle signup error
+    if (error.response.status === 409) {
+      // Username taken
+      displayErrorMessage("Username is already taken, please choose another one.");
+    } else {
+      // Other error
+      displayErrorMessage("An error has occurred, please try again later.");
+    }
+  }
+}
+
+function displayErrorMessage(message) {
+  // Display error message to the user
+  // You can create an HTML element to show the error message and append it to the appropriate section of the page.
+}
+
+/** Handle signup form submission. */
+
+async function signup(evt) {
+  console.debug("signup", evt);
+  evt.preventDefault();
+
+  const name = $("#signup-name").val();
+  const username = $("#signup-username").val();
+  const password = $("#signup-password").val();
+
+  // User.signup retrieves user info from API and returns User instance
+  // which we'll make the globally-available, logged-in user.
+  currentUser = await User.signup(username, password, name);
+
+  saveUserCredentialsInLocalStorage();
+  updateUIOnUserLogin();
+
+  $signupForm.trigger("reset");
+}
+
+$signupForm.on("submit", signup);
+
+// Update user profile
+
+async function updateUserProfile(evt) {
+  console.debug("updateUserProfile", evt);
+  evt.preventDefault();
+
+  const newName = $("#user-profile-name").val();
+  const newPassword = $("#user-profile-password").val();
+
+  try {
+    await currentUser.updateProfile(newName, newPassword);
+    // Display success message or update UI accordingly
+  } catch (error) {
+    // Handle update error
+    displayErrorMessage("An error occurred while updating the profile. Please try again later.");
+  }
+}
+
+$("#user-profile-form").on("submit", updateUserProfile);
+
+
+/** Handle click of logout button
+ *
+ * Remove their credentials from localStorage and refresh page
+ */
+
+function logout(evt) {
+  console.debug("logout", evt);
+  localStorage.clear();
+  location.reload();
+}
+
+$navLogOut.on("click", logout);
+
+/******************************************************************************
+ * Storing/recalling previously-logged-in-user with localStorage
+ */
+
+/** If there are user credentials in local storage, use those to log in
+ * that user. This is meant to be called on page load, just once.
+ */
+
+async function checkForRememberedUser() {
+  console.debug("checkForRememberedUser");
+  const token = localStorage.getItem("token");
+  const username = localStorage.getItem("username");
+  if (!token || !username) return false;
+
+  // try to log in with these credentials (will be null if login failed)
+  currentUser = await User.loginViaStoredCredentials(token, username);
+}
+
+/** Sync current user information to localStorage.
+ *
+ * We store the username/token in localStorage so when the page is refreshed
+ * (or the user revisits the site later), they will still be logged in.
+ */
+
+function saveUserCredentialsInLocalStorage() {
+  console.debug("saveUserCredentialsInLocalStorage");
+  if (currentUser) {
+    localStorage.setItem("token", currentUser.loginToken);
+    localStorage.setItem("username", currentUser.username);
+  }
+}
+
+/******************************************************************************
+ * General UI stuff about users
+ */
+
+/** When a user signs up or registers, we want to set up the UI for them:
+ *
+ * - show the stories list
+ * - update nav bar options for logged-in user
+ * - generate the user profile part of the page
+ */
+
+function updateUIOnUserLogin() {
+  console.debug("updateUIOnUserLogin");
+
+  $allStoriesList.show();
+
+  updateNavOnLogin();
+}
